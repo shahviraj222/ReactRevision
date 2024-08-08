a684c3e4d47fe5f120cf36b417c496514ee7708f
shahviraj222 <viraj22feb@gmail.com>
2024-08-08 23:16:37 +0530

    todo done there is problem the localstorage is not stored

--- React/06todolist/src/App.jsx
+++ React/06todolist/src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { TodoProvider } from "./contexts/TodoContext"
+import TodoForm from "./components/TodoForm"
+import TodoItem from "./components/TodoItem"
 
 
 function App() {
-  
+
   const [todos,setTodo] = useState([])
 
   const addTodo = (todo)=>{
@@ -25,16 +27,34 @@ function App() {
     setTodo((prev) => prev.map((eachtodo) => eachtodo.id == id ? {...eachtodo,completed:!eachtodo.completed} : eachtodo))
   }
 
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    console.log(todos)
+    if(todos)
+    {
+      setTodo(todos)
+    }
+  },[])
+
+  useEffect(() => {
+    localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos,setTodo])
+
+  useEffect(()=>{
+    console.log(todos)
+  },[todos,setTodo])
   return (
     <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
     <div className="bg-[#172842] min-h-screen py-8">
       <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
         <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
         <div className="mb-4">
-          {/* Todo form goes here */}
+          <TodoForm/>
         </div>
         <div className="flex flex-wrap gap-y-3">
-          {/*Loop and Add TodoItem here */}
+          {
+            todos.map((todo)=><TodoItem key={todo.id} todo={todo}/>)
+          }
         </div>
       </div>
     </div>
