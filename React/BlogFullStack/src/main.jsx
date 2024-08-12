4dd0908b66e8ed51ebdebd435cd200949685b1cb
shahviraj222 <viraj22feb@gmail.com>
2024-08-12 15:41:13 +0530

    adding router

--- React/BlogFullStack/src/main.jsx
+++ React/BlogFullStack/src/main.jsx
@@ -3,12 +3,20 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
+import { RouterProvider ,createBrowserRouter} from 'react-router-dom'
 import store from './store/store.js'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />},
+  ]
+  )
 createRoot(document.getElementById('root')).render(
   
   <StrictMode>
     <Provider store={store}>
-      <App />
+    <RouterProvider router={router}/>
     </Provider>
   </StrictMode>,
 )
