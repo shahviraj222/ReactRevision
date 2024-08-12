f461c80e2c5480646af671f858bad3f78dd27986
shahviraj222 <viraj22feb@gmail.com>
2024-08-12 15:42:44 +0530

    update

--- React/BlogFullStack/src/components/index.js
+++ React/BlogFullStack/src/components/index.js
@@ -1,8 +1,13 @@
 //in appjs to use compoents you only has to import the index.js and use compoents
 import Header from "./header";
 import Footer from "./footer";
-
+import Logo from "./Logo";
+import Container from "./Container";
+import Logoutbtn from "./Logoutbtn";
 export {
     Header,
-    Footer
+    Footer,
+    Logo,
+    Container,
+    Logoutbtn
 }
