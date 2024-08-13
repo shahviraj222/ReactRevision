8a561ab02ddc333fc7715252a14db1fff0bdf68a
shahviraj222 <viraj22feb@gmail.com>
2024-08-13 23:17:01 +0530

    destructring the value pass to the PostCard

--- React/BlogFullStack/src/pages/Home.jsx
+++ React/BlogFullStack/src/pages/Home.jsx
@@ -1,41 +1,52 @@
 import React, { useState, useEffect } from 'react';
-import appwriteService from '../appwrite/configappwrite'
+import appwriteService from '../appwrite/configappwrite';
 import { Container, PostCard } from '../components';
+
 function Home(props) {
-    const [posts, setPost] = useState([])
+    const [posts, setPosts] = useState([]);
+
     useEffect(() => {
-        appwriteService.getPosts().then((posts) => {
-            if (posts) {
-                setPost(posts.documents)
+        const fetchPosts = async () => {
+            try {
+                const response = await appwriteService.getPosts();
+                if (response && response.documents) {
+                    setPosts(response.documents);
+                } else {
+                    console.error('No documents found.');
+                }
+            } catch (error) {
+                console.error('Error fetching posts:', error);
             }
-        })
-    }, [])
+        };
+
+        fetchPosts();
+    }, []);
 
     if (posts.length === 0) {
         return (
-            <div>
-                <Container>
-                    <h1>Login to read Post</h1>
-                </Container>
-            </div>
-        )
+            <Container>
+                <h1>Login to read Post</h1>
+            </Container>
+        );
     }
 
     return (
         <div className='w-full py-8'>
             <Container>
                 <div className='flex flex-wrap'>
-                    {
-                        posts.map((post) => (
-                            <div key={post.$id} className='p-2 w-1/4'>
-                                <PostCard post={post} />
-                            </div>
-                        ))
-                    }
+                    {posts.map((post) => (
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <PostCard
+                                $id={post.$id}
+                                title={post.title}
+                                featuredImage={post.featuredImage}
+                            />
+                        </div>
+                    ))}
                 </div>
             </Container>
         </div>
-    )
+    );
 }
 
 export default Home;
