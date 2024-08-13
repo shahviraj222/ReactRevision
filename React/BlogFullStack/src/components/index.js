8f7b993cecd1129f49bdaf2ae469aa98c37506d2
shahviraj222 <viraj22feb@gmail.com>
2024-08-13 19:40:47 +0530

    all pages are done

--- React/BlogFullStack/src/components/index.js
+++ React/BlogFullStack/src/components/index.js
@@ -6,6 +6,11 @@ import Container from "./Container";
 import Logoutbtn from "./Logoutbtn";
 import Button from './Button'
 import Input from './Input'
+import RTE from "./RTE";
+import PostForm from './PostForm'
+import Login from "./Login";
+import Signup from "./Singup";
+import PostCard from './PostCard'
 export {
     Header,
     Footer,
@@ -13,5 +18,10 @@ export {
     Container,
     Logoutbtn,
     Input,
-    Button
+    Button,
+    RTE,
+    PostForm,
+    Signup,
+    Login,
+    PostCard
 }
---
+++ React/BlogFullStack/src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import appwriteService from '../appwrite/configappwrite'
+import { Container, PostCard } from '../components';
+function Home(props) {
+    const [posts, setPost] = useState([])
+    useEffect(() => {
+        appwriteService.getPosts().then((posts) => {
+            if (posts) {
+                setPost(posts.documents)
+            }
+        })
+    }, [])
+
+    if (posts.length) {
+        return (
+            <div>
+                <Container>
+                    <h1>Login to read Post</h1>
+                </Container>
+            </div>
+        )
+    }
+
+    return (
+        <div className='w-full py-8'>
+            <Container>
+                <div className='flex flex-wrap'>
+                    {
+                        posts.map((post) => (
+                            <div key={post.$id} className='p-2 w-1/4'>
+                                <PostCard post={post} />
+                            </div>
+                        ))
+                    }
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default Home;
