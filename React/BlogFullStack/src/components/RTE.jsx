5cc06eb923f60b15ad6365b09013f79d39249a0b
shahviraj222 <viraj22feb@gmail.com>
2024-08-14 20:00:19 +0530

    adding api

--- React/BlogFullStack/src/components/RTE.jsx
+++ React/BlogFullStack/src/components/RTE.jsx
@@ -13,6 +13,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
                 control={control}
                 render={({ field: { onChange } }) => (
                     <Editor
+                        apiKey='wr1346s97nayq4b11lgu1vguo601ilv7k81v674tex33sfr4'
                         initialValue={defaultValue}
                         init={{
                             initialValue: defaultValue,
