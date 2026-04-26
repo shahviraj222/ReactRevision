679c9000246a1798f09794fb8b1f025eecf1ad11
shahviraj222 <viraj22feb@gmail.com>
2026-04-26 17:16:02 +0530

    changes

--- Javascript/Advance/arrowAdvance.js
+++ Javascript/Advance/arrowAdvance.js
@@ -17,10 +17,27 @@ person2 = {
 person.greet();
 person2.greet();
 
+// arrrow funcitonexp2
+// arrow funciton uses lexical this
+
+name = "viraj";
+
+person3 = {
+  name: "viraj",
+  calss: "9",
+  greet: {
+    add: () => {
+      console.log(this.name);
+    },
+  },
+};
+
+person3.greet.add();
+
 function test() {
   console.log(arguments);
 }
 
-const test = () => {
+const test2 = () => {
   console.log(arguments); // ❌ error
 };
