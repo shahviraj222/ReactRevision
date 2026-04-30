0112fd55637f07b40ecfb1bb4226bc78fa69f9c1
shahviraj222 <viraj22feb@gmail.com>
2026-04-30 16:18:51 +0530

    debounce function

---
+++ Javascript/Advance/debounce.js
@@ -0,0 +1,31 @@
+// debounce function is used in search bar and others also
+
+function debounce(func, delay) {
+  let timer;
+  return function (...arg) {
+    clearTimeout(timer); // ❌ cancel previous timer
+    timer = setTimeout(() => {
+      func.apply(this, arg); // ✅ schedule new one
+    }, delay);
+  };
+}
+
+// debouncedFunction("h")
+// debouncedFunction("he")
+// debouncedFunction("hel")
+// ...
+
+// setTimeout
+console.log("Start");
+
+// this create a function call that wait for 2 second and then runs
+setTimeout(() => {
+  console.log("Hello after 2 seconds");
+}, 2000);
+
+console.log("End");
+
+// output:
+// start
+// end
+// Hello after 2 seconds
